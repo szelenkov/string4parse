93915cd273bc19ba384a183887e6ea4ccc3faa28
Serhiy Zelenkov <zelenkov@gmail.com>
2024-10-04 21:00:46 -0700

    init checkin

---
+++ src/string4parse.cpp
@@ -0,0 +1,11 @@
+﻿// string4parse.cpp : Defines the entry point for the application.
+//
+
+#include "../include/string4parse.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello CMake." << endl;
+	return 0;
+}
